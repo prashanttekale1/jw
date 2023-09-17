4adec33df8dd51ece09f2144fd31c2730a7ead74
prashanttekale1 <145289507+prashanttekale1@users.noreply.github.com>
2023-09-18 00:27:00 +0530

    Add files via upload
    1

---
+++ Prashant.java
@@ -0,0 +1,26 @@
+public class Prashant {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 5;
+		int sum = a +  b;
+		//System.out.println(sum);
+		int diff = a - b;
+	//	System.out.println(diff);
+int mul= a*b;
+//System.out.println(mul);
+
+	int div = a/b;
+	System.out.println(div);
+	
+	
+	int ans = a * b/ a-b;
+System.out.println(ans);
+	
+	
+	
+	}
+	
+
+} 
